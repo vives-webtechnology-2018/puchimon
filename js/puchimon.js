acf9666711f694c7c76993e5511684478914f712
Sille Van Landschoot <info@sillevl.be>
2018-11-22 14:57:12 +0100

    add method to create a puchimon object

--- js/puchimon.js
+++ js/puchimon.js
@@ -1,3 +1,11 @@
 console.log('Weclome to the Puchimon App')
 
 const names = ['Jan', 'Piet', 'Pol']
+
+const createPuchimon = function (name) {
+  return {
+    name: name,
+    health: 100,
+    damage: 20
+  }
+}
